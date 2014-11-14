047443592e034531c350760b1dd829732160d3d1
Liem Dinh <flipsorry169@gmail.com>
2014-11-13 18:12:40 -0800

    Adding static template for the list view of items. Features include: scrollable product view with image, title, likes, and comments

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+// Let's get all of our query parameters
+var url = require('url');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+/* GET item-view */
+router.get('/item-view', function(req, res) {
+    res.render('item-view', getQueryParams(req));
+});
+
+router.get('/individual-item-view', function(req, res) {
+    res.render('individual-item-view', getQueryParams(req));
+});
+
+var getQueryParams = function(request) {
+    var url_parts = url.parse(request.url, true);
+    return url_parts.query;
+}
 
 module.exports = router;
