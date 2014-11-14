078f939a2f53132195f3f03c8134ed9abf661712
Alex Kavalsky <kavalsky@amazon.com>
2014-11-14 03:14:41 +0000

    Adding implementation for productService and productListService

---
+++ services/productService.js
@@ -0,0 +1,28 @@
+var products = require('../data/products.json');
+var amazonProducts = require('../data/amazon_product.json');
+
+module.exports = {
+  getProductsForProductListId: function(productListId) {
+    var productListProducts = [];
+    products.forEach(function(product, index) {
+      if (product.product_list_id == productListId) {
+        productListProducts.push(getProductDetailsForProduct(product));
+      }
+    });
+    return productListProducts;
+  },
+  getAllAmazonProducts: function() {
+    return amazonProducts;
+  }
+}
+
+var getProductDetailsForProduct = function(productObj) {
+  var productAsin = productObj.amazon_asin;
+  var amazonProductReturn;
+  amazonProducts.forEach(function(amazonProduct, index) {
+    if (amazonProduct.asin == productAsin) {
+      amazonProductReturn = amazonProduct;
+    }
+  });
+  return amazonProductReturn;
+}
