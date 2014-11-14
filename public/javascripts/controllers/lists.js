3d71bf6c35af3bf1a8abd7ccd4d84bb5d666c279
Amin Shams <ashams1026@gmail.com>
2014-11-14 07:39:59 -0800

    incoming lists view

--- public/javascripts/controllers/lists.js
+++ public/javascripts/controllers/lists.js
@@ -1,21 +1,33 @@
 app.controller('ListsCtrl', ['$scope', '$http', '$routeParams', '$location', 'navigationService',
    function($scope, $http, $routeParams, $location, navigationService) {
-      navigationService.setTitle("Your lists");
-     
-      // get lists for user
-      $http.get("/productlist/user/1").success(function(data) {
-        $scope.lists = data;
-      });
+      
+      if ($location.path().indexOf("incoming") != -1) {
+        navigationService.setTitle("Incoming lists");
+        // get lists for user
+        $http.get("/productlist/share/user/1").success(function(data) {
+          $scope.lists = data;
+        });
+      } else {
+        navigationService.setTitle("Your lists");
+        // get lists for user
+        $http.get("/productlist/user/1").success(function(data) {
+          $scope.lists = data;
+        });
+      }
+      
 
       $scope.deleteList = function(list) {
-        $http.post('/productlist/removeproductlist', { productListId: list.id }).success(function() {
-          for (i=0; i<$scope.lists.length; i++) {
-            if($scope.lists[i].id == list.id) {
-              $scope.lists.splice(i, 1);
-              break;
+        if ($location.path().indexOf("incoming") == -1) {
+          // only allow deleting on your own lists
+          $http.post('/productlist/removeproductlist', { productListId: list.id }).success(function() {
+            for (i=0; i<$scope.lists.length; i++) {
+              if($scope.lists[i].id == list.id) {
+                $scope.lists.splice(i, 1);
+                break;
+              }
             }
-          }
-        });
+          });
+        }
       };
       
       $scope.navigateToListView = function(list) {
