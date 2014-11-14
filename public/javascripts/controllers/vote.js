8c5cc34a0e9fca75d833555da036f217b0108779
Kyle Baylor <kyle221b@gmail.com>
2014-11-14 12:07:07 -0800

    Reconfigure some swipe values

--- public/javascripts/controllers/vote.js
+++ public/javascripts/controllers/vote.js
@@ -33,7 +33,7 @@ var initSwiper = function($scope, user) {
     $(".metadata" + scope.currentIndex).show();
     $(".productContainer").swipe({
       swipe: function(event, direction, distance, duration, fingerCount) {
-        if (duration > 100 && distance > 100) {
+        if (duration > 50 && distance > 50) {
           var product = scope.products[scope.currentIndex];
           console.log(product.comment);
           if (product.comment) {
